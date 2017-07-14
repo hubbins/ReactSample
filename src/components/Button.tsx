061d16b201eb1c9f86cd35f0451c758b023eb912
Sean O'Connor <sean.oconnor@morningstar.com>
2017-07-14 14:51:25 -0500

    cleanup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 type ButtonState = {}
-export type ButtonProps = { 
+type ButtonProps = { 
     onClickFunction: any 
 }
 
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export type ResultProps = {
+type ResultProps = {
     counter: number;
 }
 
